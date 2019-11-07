f704922d29f38137a26e5ffabd024ba0b08f9d62
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2019-11-07 20:59:19 +0200

    fix #2

--- api/echo.go
+++ api/echo.go
@@ -5,5 +5,5 @@ import "github.com/kataras/iris/v12"
 // EchoHandler to be used as Handler for ECHO API
 func EchoHandler(ctx iris.Context) {
 	message := ctx.URLParam("message")
-	ctx.Text(message)
+	ctx.WriteString(message)
 }
